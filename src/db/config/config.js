f6f00cf6d3fc531a3cfae945eb8413d72af05dec
Emanuel Arroyo <emanuelarroyodev@gmail.com>
2022-04-07 09:18:44 -0300

    heroku project

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -17,10 +17,10 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_PRODUCTION_USER,
+    password: process.env.DB_PRODUCTION_PASSWORD,
+    database: process.env.DB_PRODUCTION_NAME,
+    host: process.env.DB_PRODUCTION_HOST,
     dialect: 'mysql',
   },
 };
